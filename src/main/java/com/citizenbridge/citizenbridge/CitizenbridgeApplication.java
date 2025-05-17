d1f4df6b98358043071d9c1eb11a134821cf147d
Virgile1k <virgilendayambaje@gmail.com>
2025-05-17 10:02:06 +0200

    project initialization

---
+++ src/main/java/com/citizenbridge/citizenbridge/CitizenbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.citizenbridge.citizenbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citizenbridge/citizenbridge/CitizenbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizenbridge.citizenbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
